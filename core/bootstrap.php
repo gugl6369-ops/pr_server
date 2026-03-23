c37d706e33b698824a72096d5166da233562c14f
elfi <gugl6369@gmail.com>
2026-03-23 22:14:44 +0700

    add: full

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,34 +1,22 @@
 <?php
-
+//Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 
-
-spl_autoload_register(function ($className) {
-   $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-   $className = str_replace('\\', '/', $className);
-
-   foreach ($paths['classes'] as $path) {
-       $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-       if (file_exists($fileName)) {
-           require_once $fileName;
-       }
-   }
-});
+//Подключение автозагрузчика composer
+require_once __DIR__ . '/../vendor/autoload.php';
 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
 {
-   $settings = [];
-   foreach (scandir(__DIR__ . $path) as $file) {
-       $name = explode('.', $file)[0];
-       if (!empty($name)) {
-           $settings[$name] = include __DIR__ . "$path/$file";
-       }
-   }
-   return $settings;
+    $settings = [];
+    foreach (scandir(__DIR__ . $path) as $file) {
+        $name = explode('.', $file)[0];
+        if (!empty($name)) {
+            $settings[$name] = include __DIR__ . "$path/$file";
+        }
+    }
+    return $settings;
 }
-
-return new Src\Application(new Src\Settings(getConfigs()));
 
 require_once __DIR__ . '/../routes/web.php';
 
