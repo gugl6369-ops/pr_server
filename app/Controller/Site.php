745c2ccaccb67a8424297ca87b345ae12d7188e9
elfi <gugl6369@gmail.com>
2026-03-23 21:39:12 +0700

    add: development of the view

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,15 +1,19 @@
 <?php
+
 namespace Controller;
+
+use Src\View;
 
 class Site
 {
-   public function index(): void
-   {
-       echo 'working index';
-   }
+    public function index(): string
+    {
+        $view = new View();
+        return $view->render('site.hello', ['message' => 'index working']);
+    }
 
-   public function hello(): void
-   {
-       echo 'working hello';
-   }
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello working']);
+    }
 }
