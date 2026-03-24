68a55fa6cc87865669a5ba0821aa0f23300c80e3
elfi <gugl6369@gmail.com>
2026-03-24 12:13:02 +0700

    add: Registration and authentication functions

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -8,15 +8,14 @@ use Src\Request;
 
 class Site
 {
-
-    public function hello(): string
-    {
-        return new View('site.hello', ['message' => 'hello working']);
-    }
-
     public function index(Request $request): string
     {
         $posts = Post::where('id', $request->id)->get();
         return (new View())->render('site.post', ['posts' => $posts]);
+    }
+
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello working']);
     }
 }
