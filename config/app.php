b972292c422e81096f170596ddd4e8280b5a25ae
elfi <gugl6369@gmail.com>
2026-05-26 12:40:39 +0700

    6: api create (method)

--- config/app.php
+++ config/app.php
@@ -14,6 +14,13 @@ return [
         'csrf' => \Middlewares\CSRFMiddleware::class,
         'trim' => \Middlewares\TrimMiddleware::class,
         'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+        'json' => \Middlewares\JSONMiddleware::class,
+    ],
+    'providers' => [
+        'kernel' => \Providers\KernelProvider::class,
+        'route' => \Providers\RouteProvider::class,
+        'db' => \Providers\DBProvider::class,
+        'auth' => \Providers\AuthProvider::class,
     ],
 
 
